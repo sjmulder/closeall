fda18376ff1df868ca06bf780622c5182ee3d9aa
Sijmen J. Mulder <ik@sjmulder.nl>
2017-08-07 00:37:19 +0200

    Clean up build_gcc.cmd

--- build_gcc.cmd
+++ build_gcc.cmd
@@ -1,9 +1,7 @@
-@echo off
-
-set CC=gcc
-set CFLAGS=-ansi -Wall -nostdlib -ffreestanding -Os -s
-set LDFLAGS=-mwindows -s
-set LDLIBS=-lkernel32 -luser32
-set TARGET=closeall.exe
+@set CC=gcc
+@set CFLAGS=-ansi -Wall -nostdlib -ffreestanding -Os -s
+@set LDFLAGS=-mwindows -s
+@set LDLIBS=-lkernel32 -luser32
+@set TARGET=closeall.exe
 
 %CC% %CFLAGS% %LDFLAGS% -o %TARGET% *.c %LDLIBS%
