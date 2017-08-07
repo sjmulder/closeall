90a70323fb0d005c96f4bacadde2cf408e8c6f35
Sijmen J. Mulder <ik@sjmulder.nl>
2017-08-07 02:05:52 +0200

    Add version resource

--- build_cl.cmd
+++ build_cl.cmd
@@ -1,7 +1,7 @@
-@set CC=cl
 @set CFLAGS=/nologo /O1
 @set LDFLAGS=/nodefaultlib /subsystem:windows /entry:WinMainCRTStartup
 @set LDLIBS=kernel32.lib user32.lib
-@set TARGET=closeall.exe
 
-%CC% %CFLAGS% /Fe:%TARGET% *.c /link %LDFLAGS% %LDLIBS%
+rc resources.rc
+cl %CFLAGS% /Fe:closeall.exe *.c resources.res /link %LDFLAGS% %LDLIBS%
+del resources.res
